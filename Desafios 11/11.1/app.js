347f568b1829c8f7a6539df43489f7b2d34fee5f
Lautaro Brunetto <laubrunetto1998@gmail.com>
2022-07-04 20:26:46 -0300

    Desafios 10 y 11

---
+++ Desafios 11/11.1/app.js
@@ -0,0 +1,12 @@
+
+
+
+let costosFijos = prompt("Ingresar costos fijos")
+
+let precioDeVenta = prompt("Ingresar precio de venta")
+
+let costosVariables = prompt("Ingresar costos variables")
+
+let puntoDeEquilibrio = (costosFijos / (precioDeVenta - costosVariables)  )
+
+alert("El punto de equilibrio es " + " " + puntoDeEquilibrio)
---
+++ Desafios 11/11.2/app.js
@@ -0,0 +1,11 @@
+
+
+let ingresosTotales = prompt("Ingresos totales =");
+
+let gastosNecesarios = (ingresosTotales * 0.5);
+
+let gastosOpcionales = (ingresosTotales * 0.3);
+
+let ahorros = (ingresosTotales * 0.2);
+
+alert(" Presupuesto: Gastos Necesarios =" + gastosNecesarios  + " " + " Gastos opcionales = " + gastosOpcionales + " " + "Ahoros = " + ahorros);
