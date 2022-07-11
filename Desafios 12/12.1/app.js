497f631000242504770342d2c6115c9db97c85b1
Lautaro Brunetto <laubrunetto1998@gmail.com>
2022-07-11 14:51:45 -0300

    Desafio 12

---
+++ Desafios 12/12.1/app.js
@@ -0,0 +1,10 @@
+
+let edad = prompt("Ingresar edad")
+
+if(edad >= 18){
+    alert("Bienvenido, el usuario es mayor de edad")
+}else{
+    alert("Error")
+}
+
+
---
+++ Desafios 12/12.2/app.js
@@ -0,0 +1,8 @@
+let Usuario = prompt("Nombre de usuario:");
+let Clave = prompt("Contraseña:");
+
+if(Usuario == "admin" && Clave == "1234") {
+    alert("Bienvenido/a");
+} else {
+    alert("Error");
+}
